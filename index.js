988abe0127b9a628b9c0783a364e6ac81792b178
bpkadarief <bpkadarief@gmail.com>
2025-06-30 08:50:21 +0700

    commit30juni

---
+++ index.js
@@ -0,0 +1,63 @@
+
+// latihan 1 //
+let nama = "John";
+let umur = 30;
+let pekerjaan = "Programmer";
+
+console.log("Nama:", nama);
+console.log("Umur:", umur);   
+console.log("Pekerjaan:", pekerjaan);
+
+
+// latihan 2 //
+let angka1 = 10;
+let angka2 = 5;
+
+console.log("Penjumlahan:", angka1 + angka2);
+console.log("Pengurangan:", angka1 - angka2); 
+
+
+// latihan 3 //
+let x = 10;
+
+// Tambah 5
+x += 5;
+console.log("Setelah ditambah 5:", x);
+
+// Kurang 5
+x -= 5;
+console.log("Setelah dikurang 5:", x);
+
+// Kali 5
+x *= 5;
+console.log("Setelah dikali 5:", x);
+
+// Bagi 5
+x /= 5;
+console.log("Setelah dibagi 5:", x); 
+
+//Latihan 4 //
+let a = 10; 
+let b = 3
+
+console.log("Hasil bagi a dan b:", a / b);
+console.log("Hasil sisa bagi a dan b:", a % b);
+console.log("increment a:", ++a);
+console.log("decrement b:", --b);
+
+// Latihan 5 //
+let isAdult = true;
+let hasLicense = false;
+
+console.log(isAdult && hasLicense); // false
+console.log(isAdult || hasLicense); // true
+
+
+//Latihan-Latihan //
+
+let message = "Hello, alert!";
+alert(message);
+
+
+
+
